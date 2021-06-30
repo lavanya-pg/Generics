4b43947c143aa2d279fc8dfc07509b56515e871e
lavanya-pg <lavanyapg8@gmail.com>
2021-06-30 20:39:27 +0530

    ADDED:To Find Maximum number 3 Integers

--- src/generics/PrintArray.java
+++ src/generics/PrintArray.java
@@ -4,7 +4,13 @@ public class PrintArray
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to the Genrics Methods Program");
+		int a = 60, b= 36, c= 28;
+		if ( a>b && a>c )
+		System.out.println(a+ " is the largest number");
+		else if ( b>a && b>c)
+		System.out.println(b+  " is the largest number");
+		else
+		System.out.println(c+  " is the largest number");
 	}
 
 }
